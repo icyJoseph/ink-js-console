3e2e9943317657e9e2bba46a3fb1efbbac8b770e
icyJoseph <sephxd1234@gmail.com>
2018-09-17 18:11:34 +0200

    test: dummy test :see_no_evil:

--- test.js
+++ test.js
@@ -1,9 +1,8 @@
-import { h, renderToString, Text } from "ink";
-import test from "ava";
-import Console from ".";
+import { h, renderToString } from 'ink';
+import test from 'ava';
+import Console from '.';
 
-// TODO: improve tests!!!!!!
-test("output", t => {
+test('output', t => {
 	const actual = renderToString(<Console lines={20} />);
 	const expected = renderToString(<Console lines={20} />);
 
